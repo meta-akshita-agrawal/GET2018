3b12f86a79adafb2f9fde9e2d436fb044e17b4ad
meta-akshita-agrawal <akshita.agrawal@metacube.com>
2018-07-18 18:21:40 +0530

    hexa and job scheduler commit

---
+++ PF/PF-Assignment2/JobScheduler.java
@@ -0,0 +1,99 @@
+import java.util.Scanner;
+
+public class JobScheduler {
+
+	// Returns turn around time of a process.
+	public static int turnAroundTime(int completionTime, int arrivalTime) {
+		int turnAroundTime = Math.abs(completionTime - arrivalTime);
+		return turnAroundTime;
+	}
+
+	//Returns completion time of a process.
+	public static int completionTime(int index, int[][] processArray) {
+		int completionTime = 0;
+		for (int i = 0; i <= index; i++) {
+			completionTime += processArray[i][1];
+		}
+		return completionTime;
+	}
+
+	//Returns waiting time of a process
+	public static int waitingTime(int turnAroundTime, int burstTime) {
+		int waitingTime = Math.abs(turnAroundTime - burstTime);
+		return waitingTime;
+	}
+
+	//Returns maximum waiting time of processes
+	public static int maxWaitingTime(int[] waitingTime) {
+		int maximum = waitingTime[0];
+		for (int i = 0; i < waitingTime.length; i++) {
+			if (waitingTime[i] > maximum) {
+				maximum = waitingTime[i];
+			}
+		}
+		return maximum;
+	}
+
+	//Returns average time of processes.
+	public static double averageWait(int[] waitingTime) {
+		double average = 0;
+		for (int i = 0; i < waitingTime.length; i++) {
+			average += waitingTime[i];
+		}
+		average = average / waitingTime.length;
+		return average;
+	}
+
+	
+	
+	public static void main(String args[]) {
+		int[][] processArray;
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number of processes you have.");
+
+		int size = sc.nextInt();
+		
+		
+		//Initializing array
+		processArray = new int[size][2];
+
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < 2; j++) {
+				processArray[i][j] = sc.nextInt();
+			}
+		}
+
+		//Initializing each array for turn around, waiting, completion time
+		int[] turnAroundTime = new int[size];
+		int[] completionTime = new int[size];
+		int[] waitingTime = new int[size];
+		completionTime[0] = 0;
+
+		for (int i = 0; i < size; i++) {
+			completionTime[i] += completionTime(i, processArray);
+			turnAroundTime[i] = turnAroundTime(completionTime[i],
+					processArray[i][0]);
+			waitingTime[i] = waitingTime(turnAroundTime[i], processArray[i][1]);
+		}
+
+		//Printing out.
+		
+		System.out.println("PID\tAT\tBT\tCT\tTAT\tWT");
+		System.out.println("-------------------------------------");
+
+		for (int i = 0; i < size; i++) {
+			System.out.println(i + "\t" + processArray[i][0] + "\t"
+					+ processArray[i][1] + "\t" + completionTime[i] + "\t"
+					+ turnAroundTime[i] + "\t" + waitingTime[i]);
+		}
+
+		System.out.println("-------------------------------------");
+
+		System.out.println("AVERAGE WAITING TIME: " + averageWait(waitingTime));
+		System.out.println("MAXIMUM WAITING TIME: "
+				+ maxWaitingTime(waitingTime));
+		sc.close();
+
+	}
+}
