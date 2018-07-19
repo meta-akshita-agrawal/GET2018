e9910af273e62b8328869b934e425ead4d034ad1
meta-akshita-agrawal <41181785+meta-akshita-agrawal@users.noreply.github.com>
2018-07-19 10:01:04 +0530

    static change

--- PF/PF-Assignment2/JobScheduler.java
+++ PF/PF-Assignment2/JobScheduler.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class JobScheduler {
 
 	// Returns turn around time of a process.
-	public static int turnAroundTime(int completionTime, int arrivalTime) {
+	public int turnAroundTime(int completionTime, int arrivalTime) {
 		int turnAroundTime = Math.abs(completionTime - arrivalTime);
 		return turnAroundTime;
 	}
 
 	//Returns completion time of a process.
-	public static int completionTime(int index, int[][] processArray) {
+	public int completionTime(int index, int[][] processArray) {
 		int completionTime = 0;
 		for (int i = 0; i <= index; i++) {
 			completionTime += processArray[i][1];
@@ -18,13 +18,13 @@ public class JobScheduler {
 	}
 
 	//Returns waiting time of a process
-	public static int waitingTime(int turnAroundTime, int burstTime) {
+	public int waitingTime(int turnAroundTime, int burstTime) {
 		int waitingTime = Math.abs(turnAroundTime - burstTime);
 		return waitingTime;
 	}
 
 	//Returns maximum waiting time of processes
-	public static int maxWaitingTime(int[] waitingTime) {
+	public int maxWaitingTime(int[] waitingTime) {
 		int maximum = waitingTime[0];
 		for (int i = 0; i < waitingTime.length; i++) {
 			if (waitingTime[i] > maximum) {
@@ -35,7 +35,7 @@ public class JobScheduler {
 	}
 
 	//Returns average time of processes.
-	public static double averageWait(int[] waitingTime) {
+	public double averageWait(int[] waitingTime) {
 		double average = 0;
 		for (int i = 0; i < waitingTime.length; i++) {
 			average += waitingTime[i];
@@ -46,9 +46,10 @@ public class JobScheduler {
 
 	
 	
-	public static void main(String args[]) {
+	public void main(String args[]) {
 		int[][] processArray;
 
+		JobScheduler jsobj = new JobScheduler();
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter number of processes you have.");
 
@@ -71,10 +72,10 @@ public class JobScheduler {
 		completionTime[0] = 0;
 
 		for (int i = 0; i < size; i++) {
-			completionTime[i] += completionTime(i, processArray);
-			turnAroundTime[i] = turnAroundTime(completionTime[i],
+			completionTime[i] += jsobj.completionTime(i, processArray);
+			turnAroundTime[i] = jsobj.turnAroundTime(completionTime[i],
 					processArray[i][0]);
-			waitingTime[i] = waitingTime(turnAroundTime[i], processArray[i][1]);
+			waitingTime[i] = jsobj.waitingTime(turnAroundTime[i], processArray[i][1]);
 		}
 
 		//Printing out.
@@ -90,9 +91,9 @@ public class JobScheduler {
 
 		System.out.println("-------------------------------------");
 
-		System.out.println("AVERAGE WAITING TIME: " + averageWait(waitingTime));
+		System.out.println("AVERAGE WAITING TIME: " + jsobj.averageWait(waitingTime));
 		System.out.println("MAXIMUM WAITING TIME: "
-				+ maxWaitingTime(waitingTime));
+				+ jsobj.maxWaitingTime(waitingTime));
 		sc.close();
 
 	}
