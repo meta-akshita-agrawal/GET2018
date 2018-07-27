57aa84b87cc6fc34285a7a3eaae4d0cabeacf9cc
meta-akshita-agrawal <41181785+meta-akshita-agrawal@users.noreply.github.com>
2018-07-27 12:01:20 +0530

    Update isMemberTest.java

--- PF/PF-Assignment6/isMemberTest.java
+++ PF/PF-Assignment6/isMemberTest.java
@@ -1,43 +1,31 @@
 import static org.junit.Assert.*;
 
-import java.util.Arrays;
-import java.util.Collection;
-
-
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 
-
-@RunWith(Parameterized.class)
 public class isMemberTest {
 
-	private IntSet obj1;
-	private int member;
-	private boolean check;
-
-	
-	public isMemberTest(IntSet obj1,int member, boolean check){
-		this.obj1 = obj1;
-		this.member = member;
-		this.check = check;
-	}
-	
-	
-	@Parameters
-	public static Collection<Object[]> input(){
-		return Arrays.asList(new Object[][]{
-				{new IntSet(new int[]{25,67,3,123}) , 67, true},
-				{new IntSet(new int[]{25,67,3,123}) , 69, false}
-				
-		});
-	}
-	
 	@Test
-	public void evaluate(){
-		assertEquals(obj1.isMember(member),check);
+	public void ordinary() {
+		IntSet obj1 = new IntSet(new int[] { 3, 25, 67 });
+		assertEquals(obj1.isMember(67), true);
 	}
-	
-}
 
+	@Test
+	public void negative() {
+		IntSet obj1 = new IntSet(new int[] { 3, 25, 67 });
+		assertNotEquals(obj1.isMember(45), true);
+	}
+
+	//set should be sorted otherwise assertion error
+	@Test(expected = AssertionError.class)
+	public void notSorted() {
+		new IntSet(new int[] { 100, 3, 25, 67 });
+	}
+
+	//set should not contain duplicates otherwise assertion error.
+	@Test(expected = AssertionError.class)
+	public void duplicates() {
+		new IntSet(new int[] { 100, 100, 25, 67 });
+	}
+
+}
