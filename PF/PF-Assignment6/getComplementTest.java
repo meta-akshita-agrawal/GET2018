7e2fea230fa5688af15dfc37d6522d2aed512b2d
meta-akshita-agrawal <41181785+meta-akshita-agrawal@users.noreply.github.com>
2018-07-27 12:02:58 +0530

    Update getComplementTest.java

--- PF/PF-Assignment6/getComplementTest.java
+++ PF/PF-Assignment6/getComplementTest.java
@@ -1,7 +1,9 @@
 import static org.junit.Assert.*;
+
 import org.hamcrest.core.IsEqual;
 import org.hamcrest.core.IsNot;
 import org.junit.Test;
+
 
 
 import java.util.stream.IntStream;
@@ -18,6 +20,7 @@ public class getComplementTest {
 		assertArrayEquals(s1.getComplement(),expectedResult);
 	}
 	
+	//actual result was set {3..1000} but expected was different 
 	@Test
 	public void negative(){
 		IntSet s1 = new IntSet(new int[]{1,2});
@@ -26,5 +29,17 @@ public class getComplementTest {
 		assertThat(s1.getComplement(), IsNot.not(IsEqual.equalTo(expectedResult)));
 	}
 	
+	//set should be sorted otherwise assertion error
+	@Test(expected = AssertionError.class)
+	public void notSorted() {
+		new IntSet(new int[] { 100, 3, 25, 67 });
+	}
+	
+	//set should not contain duplicates otherwise assertion error.
+	@Test(expected = AssertionError.class)
+	public void duplicates() {
+		new IntSet(new int[] { 100, 100, 25, 67 });
+	}
+
 }
 
