90d7b13a5a7745fbbf1b844bb567d9e640c88f42
meta-akshita-agrawal <41181785+meta-akshita-agrawal@users.noreply.github.com>
2018-08-20 16:17:15 +0530

    Update dbms4_ques3.sql

--- DBMS/DBMSAssignment4/dbms4_ques3.sql
+++ DBMS/DBMSAssignment4/dbms4_ques3.sql
@@ -1,5 +1,7 @@
 alter table `order` add index order_table_index (OrderID);
 
+alter table `order` add index order_date_index (Date);
+
 alter table `product` add index product_table_index (ProductID);
 
 alter table `category` add index category_table_index (CategoryID);
