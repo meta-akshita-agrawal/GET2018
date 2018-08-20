d4f8639e9b398473ef90fa3afc6672eb3e9dc4fb
meta-akshita-agrawal <41181785+meta-akshita-agrawal@users.noreply.github.com>
2018-08-20 16:16:13 +0530

    Update dbms4_ques2.sql

--- DBMS/DBMSAssignment4/dbms4_ques2.sql
+++ DBMS/DBMSAssignment4/dbms4_ques2.sql
@@ -1,9 +1,11 @@
+/*A Stored procedure to retrieve average sales of each product in a month.
+Month and year will be input parameter to function.*/
+
 DELIMITER $$
 
  CREATE procedure average_sales (IN sales_month int(2), IN sales_year int(4))
 	begin 
-    
-    select pn, avg(counted) from(
+   
 		select p.`ProductName` as pn, count(*) as counted
 		from `product` p
 		inner join `ordered_product` op
@@ -11,20 +13,22 @@ DELIMITER $$
 		inner join `order` o
 		on op.`OrderID` = o.`OrderID`
 		where month(o.`Date`) = sales_month and year(o.`Date`) = sales_year
-		group by p.`ProductID`) as counts
-    group by pn;
+		group by p.`ProductID`
     
 	end;
     
     
 call average_sales(1,2018);
 
+/*A stored procedure to retrieve table having order detail with status for a given period.
+Start date and end date will be input parameter.*/
+									  
 DELIMITER $$
 
 CREATE procedure order_detail(in start_date date, in end_date date)
 	
     begin
-    
+    /*If start date is greater than end date take first date of month as start date.*/
     if start_date > end_date then
 		set start_date = DATE_ADD(DATE_ADD(LAST_DAY(end_date),
                 INTERVAL 1 DAY),
