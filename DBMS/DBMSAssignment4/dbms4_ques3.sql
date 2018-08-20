5d912e7148b3d3ae98c3f53830fe606fc1b28a57
meta-akshita-agrawal <41181785+meta-akshita-agrawal@users.noreply.github.com>
2018-08-20 16:18:16 +0530

    Update dbms4_ques3.sql

--- DBMS/DBMSAssignment4/dbms4_ques3.sql
+++ DBMS/DBMSAssignment4/dbms4_ques3.sql
@@ -1,3 +1,5 @@
+/*Identifies the columns require indexing in order, product, category tables and creates indexes.*/
+
 alter table `order` add index order_table_index (OrderID);
 
 alter table `order` add index order_date_index (Date);
